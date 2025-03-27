73634e2426ee8f5afbee10f804b2860f1027f021
KshitijBhatia2005 <bhatiak727@gmail.com>
2025-03-27 23:42:29 +0530

    Alot of fixes

--- index.js
+++ index.js
@@ -1,34 +1,31 @@
 const express = require("express");
 const http = require("http");
 const socketIo = require("socket.io");
-const cors = require("cors");
 
 const app = express();
 const server = http.createServer(app);
-const io = socketIo(server, {
-  cors: {
-    origin: "*",
-    methods: ["GET", "POST"]
-  }
-});
+const io = socketIo(server);
 
-// Serve static files from the public folder
 app.use(express.static("public"));
 
-const PORT = process.env.PORT || 10000; 
+const PORT = process.env.PORT || 3000;
 
 io.on("connection", (socket) => {
-  console.log("A user connected");
+    console.log("A user connected");
 
-  socket.on("chatMessage", (msg) => {
-    io.emit("chatMessage", msg); // Broadcast message to all users
-  });
+    socket.on("chatMessage", (data) => {
+        socket.broadcast.emit("chatMessage", data); // Send message to others only
+    });
 
-  socket.on("disconnect", () => {
-    console.log("User disconnected");
-  });
+    socket.on("typing", () => {
+        socket.broadcast.emit("typing");
+    });
+
+    socket.on("disconnect", () => {
+        console.log("User disconnected");
+    });
 });
 
 server.listen(PORT, () => {
-  console.log(`Server running on port ${PORT}`);
+    console.log(`Server running on port ${PORT}`);
 });
